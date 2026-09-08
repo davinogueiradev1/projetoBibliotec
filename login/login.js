cb8b3992ca391dd7cdb3afd56cf987c01178ec60
DAVI NOGUEIRA DE SOUZA BARROSO RODRIGUES <41286020867@info.local>
2026-09-08 14:56:33 -0300

    :sparkles:feat:Mostrar Senha criado

--- login/login.js
+++ login/login.js
@@ -1,6 +1,18 @@
 const campoSenha = document.getElementById("senha")
 const btnSenha = document.querySelector("#mostrar-senha")
 
+console.log(campoSenha.type)
+console.log(btnSenha.type)
 btnSenha.addEventListener("click", function(){
-    alert("Cliquei no botão!")
+
+    // if(campoSenha.type == "password"){
+    //     campoSenha.type = "text";
+    // }
+    // 
+    // else{
+    //     campoSenha.type = "password";
+    // }
+
+    // campoSenha.type recebe uma informaçao, 
+    campoSenha.type = campoSenha.type == "password" ? "texto" : "password"
 })
